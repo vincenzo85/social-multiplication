bddd1f8d02d6037a47cd402a5d8e28ad93c52152
vincenzo di franco <vincenzo.difranco@gmail.com>
2019-02-17 21:19:08 +0100

    dont run contex!
    In this example I have removed @RunWith annotation e @SpringBootTest when is possible better do tests without ctx

--- src/test/java/microservice/book/socialmultiplication/RandomGeneratorServiceTest.java
+++ src/test/java/microservice/book/socialmultiplication/RandomGeneratorServiceTest.java
@@ -6,25 +6,30 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class RandomGeneratorServiceTest {
 
+	private RandomGeneratorService rndService;
 	
+	@Before
+	 public void setUp() {
+		rndService = new RandoGeneratorServiceImpl();
+		
 	
-	@Autowired
-	 private RandomGeneratorService rndService;
+	 }
 	
 	@Test
 	public void generateRandomFactorIsBeetweenExpetcetedLimitis() {
 		List<Integer> randomFactors = IntStream.range(0, 1000)
-				 .map(i -> rndService.
+				 .map(i -> this.rndService.
 				generateRandomFactor())
 				 .boxed().collect(Collectors.toList());
 		
